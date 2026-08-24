4385366970ce2f4b0d05e587556849ccd08cf20c
Jake <jbruder92786@gmail.com>
2026-08-24 09:22:26 -0600

    feat: add revenue trend queries

--- sql/03_exploratory_data_analysis.sql
+++ sql/03_exploratory_data_analysis.sql
@@ -15,4 +15,18 @@ SELECT
 	ROUND(AVG(item_price), 2) AS avg_item_price,
 	ROUND(SUM(item_price) / COUNT(DISTINCT order_id), 2) AS avg_order_value
 FROM fct_order_items;
+
+-- --------------------------------------------------
+-- 2. Montly Revenue & Order Volume Growth Trends
+-- --------------------------------------------------
+
+SELECT
+	TO_CHAR(purchase_timestamp, 'YYYY-MM') AS year_month,
+	COUNT(DISTINCT order_id) AS monthly_orders,
+	COUNT(order_item_id) AS montly_items_sold,
+	ROUND(SUM(item_price), 2) AS montly_gross_revenue,
+	ROUND(AVG(item_price), 2) AS avg_item_price
+FROM fct_order_items
+GROUP BY to_char(purchase_timestamp, 'YYYY-MM')
+ORDER BY year_month ASC;
 	
